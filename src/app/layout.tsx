3416de83537b474cd52407ed4bba344ae00d99ce
Mohammmad Kamran Shekh <kshekh@gmail.com>
2023-01-29 21:53:13 +0530

    Fisrt Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+// import './globals.css'
+import './index.scss'
 
 export default function RootLayout({
   children,
@@ -12,7 +13,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="text-red-500">{children}</body>
     </html>
   )
 }
